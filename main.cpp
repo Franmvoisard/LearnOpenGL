dbc322962f52156700b2526fc7868c23153afe4a
Franco Voisard <franm.voisard@gmail.com>
2026-02-17 21:12:06 -0500

    Use EBO to draw a rectangle

--- main.cpp
+++ main.cpp
@@ -94,19 +94,30 @@ int main() {
 
     // Initialize Triangle Vertex array object and vertex buffer object
     constexpr float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         -0.5f, 0.5f, 0.0f,
+         -0.5f, -0.5f, 0.0f,
+         0.5f,  -0.5f, 0.0f,
+         0.5f, 0.5f, 0.0f
+    };
+
+    constexpr unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     // Create vertex buffer object
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Bind Element Buffer object
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Enable vertex attribute array
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -123,7 +134,8 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         // Swap buffers and poll IO
         glfwSwapBuffers(window);
@@ -131,6 +143,7 @@ int main() {
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
